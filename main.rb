8f6202d0ab46a8eb3226e14064380d05559c1f5b
Alex Castillo <wikitelmex@gmail.com>
2021-12-16 07:10:12 -0700

    Update main.rb
    - implement code an decode

--- main.rb
+++ main.rb
@@ -1,1 +1,113 @@
-print 'initial setup\n'
+class Morse
+  def initialize(code, decode)
+    @morse_code = code
+    @morse_decode = decode
+  end
+
+  def coding(sentence)
+    sentence_coded = ''
+    sentence.chars.each do |letter|
+      sentence_coded = "#{sentence_coded}#{@morse_code[letter]} "
+    end
+    return sentence_coded
+  end
+
+  def decoding(sentence_coded)
+    sentence_decoded = ''
+    sentence_coded.gsub!('  ', ' _ ')
+    sentence_coded.split.each do |letter|
+      sentence_decoded += @morse_decode[letter]
+    end
+    return sentence_decoded
+  end
+end
+morse_code = {
+  'a' => '.-',
+  'b' => '-...',
+  'c' => '-.-.',
+  'd' => '-..',
+  'e' => '.',
+  'f' => '..-.',
+  'g' => '--.',
+  'h' => '....',
+  'i' => '..',
+  'j' => '.---',
+  'k' => '-.-',
+  'l' => '.-..',
+  'm' => '--',
+  'n' => '-.',
+  'o' => '---',
+  'p' => '.--.',
+  'q' => '--.-',
+  'r' => '.-.',
+  's' => '...',
+  't' => '-',
+  'u' => '..-',
+  'v' => '...-',
+  'w' => '.--',
+  'x' => '-..-',
+  'y' => '-.--',
+  'z' => '--..',
+  ' ' => '',
+  '1' => '.----',
+  '2' => '..---',
+  '3' => '...--',
+  '4' => '....-',
+  '5' => '.....',
+  '6' => '-....',
+  '7' => '--...',
+  '8' => '---..',
+  '9' => '----.',
+  '0' => '-----'
+}
+
+morse_decode = {
+  '.-' => 'a',
+  '-...' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z',
+  '  ' => ' ',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9',
+  '----- ' => '0',
+  '_' => ' '
+}
+
+morse_obj = Morse.new(morse_code, morse_decode)
+
+sentence = 'my name is robert'
+
+text_coded = morse_obj.coding(sentence)
+text_decoded = morse_obj.decoding(text_coded)
+
+puts text_coded
+puts text_decoded
